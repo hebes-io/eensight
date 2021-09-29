f973bfca00e3589acb27e720634ec22c96a15b1f
Sotiris Papadelis <48722900+hebes-io@users.noreply.github.com>
2021-09-29 13:40:55 +0300

    Update __init__.py

--- src/eensight/models/__init__.py
+++ src/eensight/models/__init__.py
@@ -1,3 +1,5 @@
-from .ensemble import CalendarEnsemble, ParetoEnsemble
+from .composite import CompositePredictor
+from .ensemble import EnsemblePredictor
+from .grouped import GroupedPredictor
 from .linear import LinearPredictor
 from .seasonal import SeasonalDecomposer
